8107c4f0766cdf3dafeccfe5e76f567384cb9b2a
Lostlias <esawelias6278@gmail.com>
2024-06-20 21:16:31 +0200

    Erfolgreiche Erkennung. Noch nicht allzu sicher.

--- src/webcam_recog.py
+++ src/webcam_recog.py
@@ -17,39 +17,40 @@ model = torch.hub.load("d:\\KIT\\HackAThonWBK24\\Yolo\\yolov5", "custom", path="
 # object classes
 classNames = ["emptybox", "fullbox"]
 
-
 while True:
     success, img = cap.read()
+    if not success:
+        break
+    
+    # Run inference
     results = model(img)
 
     # coordinates
-    for r in results:
-        boxes = r.boxes
+    boxes = results.xyxy[0]  # Access the bounding boxes
 
-        for box in boxes:
-            # bounding box
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+    for box in boxes:
+        # bounding box
+        x1, y1, x2, y2 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
 
-            # put box in cam
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        # put box in cam
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-            # confidence
-            confidence = math.ceil((box.conf[0]*100))/100
-            print("Confidence --->",confidence)
+        # confidence
+        confidence = math.ceil((box[4]*100))/100
+        print("Confidence --->", confidence)
 
-            # class name
-            cls = int(box.cls[0])
-            print("Class name -->", classNames[cls])
+        # class name
+        cls = int(box[5])
+        print("Class name -->", classNames[cls])
 
-            # object details
-            org = [x1, y1]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            fontScale = 1
-            color = (255, 0, 0)
-            thickness = 2
+        # object details
+        org = (x1, y1 - 10)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 0.5
+        color = (255, 0, 0)
+        thickness = 2
 
-            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+        cv2.putText(img, f"{classNames[cls]} {confidence}", org, font, fontScale, color, thickness)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
